0470933a2a6f511dd4b51e0db5b547ff7ff62985
zhuhuizhen <zhuhuizhen@hujiang.com>
2019-05-15 15:17:28 +0800

    update: 0.2.5

--- render/index.js
+++ render/index.js
@@ -3,36 +3,34 @@ const pageCache = require('../lib/pageCache');
 const renderHtml = require('./renderHtml');
 const renderJson = require('./renderJson');
 
-var ctx = null;
-var config = null;
-
 // 统一的render函数-
-async function render(viewOrJson) {
-    // render函数不传参数时给默认值：action的名字==view的名字-
-    if (!viewOrJson) {
-        viewOrJson = ctx.controller[1];
-    }
-    // 如果是json数据，则走json输出-
-    if (typeof viewOrJson === 'object' || viewOrJson[0] === '{' || viewOrJson[0] === '[') {
-        renderJson(ctx, viewOrJson);
-    } else {
-        let viewPath = viewOrJson;
-        if (!viewPath.includes('/') && !viewPath.includes('\\')) {
-            viewPath = path.join(config.root, 'views', ctx.controller[0], viewPath + '.html');
+function getRender(ctx, config) {
+    return function (viewOrJson) {
+        // render函数不传参数时给默认值：action的名字==view的名字-
+        if (!viewOrJson) {
+            viewOrJson = ctx.controller[1];
         }
-        // 不存在view文件，则输出json
-        if (!config.views[viewPath.toLowerCase()]) {
-            renderJson(ctx, ctx.scope);
+        // 如果是json数据，则走json输出-
+        if (typeof viewOrJson === 'object' || viewOrJson[0] === '{' || viewOrJson[0] === '[') {
+            renderJson(ctx, viewOrJson);
         } else {
-            renderHtml(ctx, config, viewPath);
+            let viewPath = viewOrJson;
+            if (!viewPath.includes('/') && !viewPath.includes('\\')) {
+                viewPath = path.join(config.root, 'views', ctx.controller[0], viewPath + '.html');
+            }
+            // 不存在view文件，则输出json
+            if (!config.views[viewPath.toLowerCase()]) {
+                renderJson(ctx, ctx.scope);
+            } else {
+                renderHtml(ctx, config, viewPath);
+            }
         }
+        pageCache.set(ctx);
     }
-    pageCache.set(ctx);
 }
 
-module.exports = async function (_config) {
-    ctx = this;
-    config = _config;
+module.exports = async function (config) {
+    let ctx = this;
 
-    ctx.render = render;
+    ctx.render = getRender(ctx, config);
 };
